c8a2540592c6f403fa839528d0ea553f6ab61bd7
Anatolyie <110687760+Anatolyie@users.noreply.github.com>
2024-09-13 18:05:10 +0200

    display genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -12,7 +12,7 @@ function GameGrid() {
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid
-        spacing={10}
+        spacing={3}
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
         padding="10px"
       >
