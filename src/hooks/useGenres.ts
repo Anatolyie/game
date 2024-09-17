9a4bd3cbeeb9bf8d4adf39c6cb71aecb2d6bda19
Anatolyie <110687760+Anatolyie@users.noreply.github.com>
2024-09-17 21:22:24 +0200

    refactoring create lookup hooks

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -14,8 +14,8 @@ const useGenres = () =>
   useQuery({
     queryKey: ["genres"],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000, //24hours
-    initialData: { count: genres.length, results: genres },
+    staleTime: 24 * 60 * 60 * 1000,
+    initialData: genres,
   });
 
 export default useGenres;
